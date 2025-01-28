6f5ede57240efd31a3c5067e95096d41914f7302
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-01-29 00:02:41 +0200

    Update StarRating.js
    Starting the dynamic section of tempRating stars

--- src/StarRating.js
+++ src/StarRating.js
@@ -14,22 +14,27 @@ export default function StarRating({ length }) {
     };
 
     const [currentRating, setCurrentRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
 
     return (
         <div style={starContainer}>
             {Array.from({ length: length }, (_, i) => (
                 <>
                     {currentRating > i ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="#fcc419"
-                            stroke="#fcc419"
-                            style={starStyle}
-                            onClick={() => setCurrentRating(i + 1)}
-                        >
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                        </svg>
+                        <span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="#fcc419"
+                                stroke="#fcc419"
+                                style={starStyle}
+                                onClick={() => setCurrentRating(i + 1)}
+                                onMouseEnter={() => setTempRating(i + 1)}
+                                onMouseLeave={() => setTempRating(0)}
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                            </svg>
+                        </span>
                     ) : (
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -39,6 +44,8 @@ export default function StarRating({ length }) {
                             style={starStyle}
                             role="button"
                             onClick={() => setCurrentRating(i + 1)}
+                            onMouseEnter={() => setTempRating(i + 1)}
+                            onMouseLeave={() => setTempRating(0)}
                         >
                             <path
                                 strokeLinecap="round"
@@ -50,7 +57,7 @@ export default function StarRating({ length }) {
                     )}
                 </>
             ))}
-            <p>{currentRating || ""}</p>
+            <p>{tempRating || currentRating || ""}</p>
         </div>
     );
 }
